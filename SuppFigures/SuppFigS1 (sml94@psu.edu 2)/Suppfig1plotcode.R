1c0a13820c58b05395bb8136efa9c2bd3c3be0f0
Scott Leighow <sml94@psu.edu>
2019-12-07 12:35:18 -0500

    removed previous versions

--- SuppFigures/SuppFigS1 (sml94@psu.edu 2)/Suppfig1plotcode.R
+++
@@ -1,9 +0,0 @@
-setwd("~/MutationFreq_ABL_IC50/Figures/Figure 2")
-#install.packages("GGally")
-library("ggplot2")
-library("GGally")
-source("ggcorplot.R")
-icdat=read.csv("Supp_fig1_data.csv",header=T,stringsAsFactors = F)
-ggpairs(icdat[,2:6],lower=list(continuous="smooth"),
-        diag=list(continuous="bar" ), 
-        upper=list(), axisLabels='show')
